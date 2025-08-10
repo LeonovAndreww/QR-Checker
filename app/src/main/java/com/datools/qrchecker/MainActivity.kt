86c25075095de2d34dd9dd08476eef420ee3fd4f
leonov <andrey_leonov2007@mail.ru>
2025-08-09 22:06:07 -0500

    Minor cosmetic changes

--- app/src/main/java/com/datools/qrchecker/MainActivity.kt
+++ app/src/main/java/com/datools/qrchecker/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.datools.qrchecker.ui.theme.QRCheckerTheme
 
@@ -21,20 +24,25 @@ class MainActivity : ComponentActivity() {
             QRCheckerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        "User",
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
+
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Your sessions, $name:",
         modifier = modifier
+            .fillMaxWidth(),
+        style = MaterialTheme.typography.titleLarge,
+        textAlign = TextAlign.Center
     )
 }
 
