f847333f17933a81a1d0b1e59f7687d99d600bc2
aveox <krekerbruh@hotmail.com>
2025-08-11 21:42:08 +0300

    feat: CreateSessionScreen button update

--- app/src/main/java/com/datools/qrchecker/MainActivity.kt
+++ app/src/main/java/com/datools/qrchecker/MainActivity.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -15,9 +23,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 //import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -51,8 +62,10 @@ fun AppNav() {
         composable("createSession") {
             CreateSessionScreen(navController)
         }
-    }
+        composable("scan") {
 
+        }
+    }
 }
 
 @Composable
@@ -82,16 +95,52 @@ fun HomeScreen(navController: NavController) {
 
 @Composable
 fun CreateSessionScreen(navController: NavController) {
-    Scaffold(
-        topBar = { }
-    ) { innerPadding ->
-        Text(
-            text = "Настройка сессии",
+    Scaffold() { innerPadding ->
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(innerPadding),
-            style = MaterialTheme.typography.displaySmall,
-            textAlign = TextAlign.Center
-        )
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Настройка сессии",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                //.padding(innerPadding),
+                style = MaterialTheme.typography.displaySmall,
+                textAlign = TextAlign.Center
+            )
+            Spacer(Modifier.height(40.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .height(80.dp),
+                shape = MaterialTheme.shapes.small
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.pdf_icon),
+                        contentDescription = "PDF Icon",
+
+                    )
+                    Text(
+                        text = "Добавить PDF файл",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun ScanScreen(navController: NavController) {
+
 }
