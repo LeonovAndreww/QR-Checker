48df031fb53d4135300e4ce74f00b6f6fd30bcd3
aveox <krekerbruh@hotmail.com>
2025-08-12 12:43:56 +0300

    feat: CreateSessionScreen file name input window style: file selection button, continue button

--- app/src/main/java/com/datools/qrchecker/MainActivity.kt
+++ app/src/main/java/com/datools/qrchecker/MainActivity.kt
@@ -1,172 +1,244 @@
-    package com.datools.qrchecker
+package com.datools.qrchecker
 
-    import android.net.Uri
-    import android.os.Bundle
-    import android.util.Log
-    import androidx.activity.ComponentActivity
-    import androidx.activity.compose.rememberLauncherForActivityResult
-    import androidx.activity.compose.setContent
-    import androidx.activity.enableEdgeToEdge
-    import androidx.activity.result.contract.ActivityResultContracts
-    import androidx.compose.foundation.Image
-    import androidx.compose.foundation.layout.Arrangement
-    import androidx.compose.foundation.layout.Column
-    import androidx.compose.foundation.layout.Row
-    import androidx.compose.foundation.layout.Spacer
-    import androidx.compose.foundation.layout.fillMaxSize
-    import androidx.compose.foundation.layout.fillMaxWidth
-    import androidx.compose.foundation.layout.height
-    import androidx.compose.foundation.layout.padding
-    import androidx.compose.material.icons.Icons
-    import androidx.compose.material.icons.filled.Add
-    import androidx.compose.material3.Button
-    import androidx.compose.material3.FabPosition
-    import androidx.compose.material3.FloatingActionButton
-    import androidx.compose.material3.Icon
-    import androidx.compose.material3.MaterialTheme
-    import androidx.compose.material3.Scaffold
-    import androidx.compose.material3.Text
-    import androidx.compose.runtime.Composable
-    import androidx.compose.ui.Alignment
-    import androidx.compose.ui.Modifier
-    import androidx.compose.ui.graphics.Color
-    import androidx.compose.ui.res.painterResource
-    import androidx.compose.ui.text.style.TextAlign
-    import androidx.compose.ui.unit.dp
-    //import androidx.compose.ui.tooling.preview.Preview
-    import androidx.navigation.NavController
-    import androidx.navigation.compose.NavHost
-    import androidx.navigation.compose.composable
-    import androidx.navigation.compose.rememberNavController
-    import com.datools.qrchecker.ui.theme.QRCheckerTheme
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.provider.OpenableColumns
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.datools.qrchecker.ui.theme.QRCheckerTheme
 
-    class MainActivity : ComponentActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            enableEdgeToEdge()
-            setContent {
-                QRCheckerTheme {
-                    AppNav()
-                }
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QRCheckerTheme {
+                AppNav()
             }
+        }
 
+    }
+}
+
+@Composable
+fun AppNav() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(navController)
+        }
+        composable("createSession") {
+            CreateSessionScreen(navController)
+        }
+        composable("scan") {
+            ScanScreen(navController)
         }
     }
+}
 
-    @Composable
-    fun AppNav() {
-        val navController = rememberNavController()
-        NavHost(
-            navController = navController,
-            startDestination = "home"
+@Composable
+fun HomeScreen(navController: NavController) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navController.navigate("createSession") },
+                containerColor = Color.Yellow,
+                contentColor = Color.Black
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Новая сессия")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+    ) { innerPadding ->
+        Text(
+            text = "Сессии",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(innerPadding),
+            style = MaterialTheme.typography.displaySmall,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun CreateSessionScreen(navController: NavController) {
+
+    val context = LocalContext.current
+    var sessionName by remember { mutableStateOf("") }
+    var selectedPdfUri by remember { mutableStateOf<Uri?>(null) }
+    var selectedPdfName by remember { mutableStateOf("") }
+
+    val documentPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.OpenDocument(),
+        onResult = { uri: Uri? ->
+            if (uri != null) {
+                selectedPdfName = getFileName(uri, context)
+                Log.d("LogCat", "Uri открытого документа: $uri")
+            }
+        }
+    )
+
+    Scaffold(content = { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            composable("home") {
-                HomeScreen(navController)
-            }
-            composable("createSession") {
-                CreateSessionScreen(navController)
-            }
-            composable("scan") {
-                ScanScreen(navController)
-            }
-        }
-    }
-
-    @Composable
-    fun HomeScreen(navController: NavController) {
-        Scaffold(
-            floatingActionButton = {
-                FloatingActionButton(
-                    onClick = { navController.navigate("createSession") },
-                    containerColor = Color.Yellow,
-                    contentColor = Color.Black
-                ) {
-                    Icon(Icons.Default.Add, contentDescription = "Новая сессия")
-                }
-            },
-            floatingActionButtonPosition = FabPosition.Center,
-        ) { innerPadding ->
             Text(
-                text = "Сессии",
+                text = "Настройка сессии",
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(innerPadding),
+                    .fillMaxWidth(),
                 style = MaterialTheme.typography.displaySmall,
                 textAlign = TextAlign.Center
             )
-        }
-    }
-
-    @Composable
-    fun CreateSessionScreen(navController: NavController) {
-        val documentPicker = rememberLauncherForActivityResult(
-            contract = ActivityResultContracts.OpenDocument(),
-            onResult = { uri: Uri? ->
-                if (uri != null) Log.d("LogCat", "Uri открытого документа: $uri")
-            }
-        )
-        Scaffold { innerPadding ->
-            Column(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = "Настройка сессии",
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    style = MaterialTheme.typography.displaySmall,
-                    textAlign = TextAlign.Center
-                )
-                Spacer(Modifier.height(40.dp))
-                Button(
-                    onClick = {
-                        documentPicker.launch(arrayOf("application/pdf"))
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 20.dp)
-                        .height(72.dp),
-                    shape = MaterialTheme.shapes.small
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Image(
-                            painter = painterResource(R.drawable.pdf_icon),
-                            contentDescription = "PDF Icon",
-
-                            )
-                        Text(
-                            text = "Добавить PDF файл",
-                            style = MaterialTheme.typography.titleMedium
-                        )
+            TextField(
+                value = sessionName,
+                onValueChange = { input ->
+                    val filtered = input.filterNot { it == '\n' }
+                    if (sessionName.isEmpty() && filtered.startsWith(" ")) {
+                        //nothing
+                    } else {
+                        sessionName = filtered
                     }
-                }
-                Spacer(modifier = Modifier.weight(1f))
-                Button(
-                    onClick = { navController.navigate("scan") },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 20.dp)
-                        .padding(vertical = 14.dp)
-                        .height(72.dp),
-                    shape = MaterialTheme.shapes.small
+                },
+                label = { Text("Имя сессии") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(Modifier.height(40.dp))
+            Button(
+                onClick = {
+                    documentPicker.launch(arrayOf("application/pdf"))
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .height(72.dp),
+                shape = MaterialTheme.shapes.small
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
                 ) {
+                    Image(
+                        painter = painterResource(R.drawable.pdf_icon),
+                        contentDescription = "PDF Icon",
+
+                        )
                     Text(
-                        text = "Продолжить",
-                        style = MaterialTheme.typography.titleMedium
+                        text = if (selectedPdfName.isNotEmpty()) {
+                            if (selectedPdfName.length > 25) {
+                                selectedPdfName.take(22) + "..."
+                            } else {
+                                selectedPdfName
+                            }
+                        } else {
+                            "Добавить PDF файл"
+                        },
+                        style = MaterialTheme.typography.titleMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
                     )
                 }
             }
-        }
-    }
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = {
+                    if (sessionName != "") {
+                        //save session as session entity
 
-    @Composable
-    fun ScanScreen(navController: NavController) {
-        // TODO
+                        navController.navigate("scan")
+                    }
+                },
+                enabled = sessionName.isNotBlank(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = if (sessionName.isNotBlank()) MaterialTheme.colorScheme.primary else Color.Gray
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .padding(vertical = 14.dp)
+                    .height(72.dp),
+                shape = MaterialTheme.shapes.small
+            ) {
+                Text(
+                    text = "Продолжить",
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+        }
+    })
+}
+
+
+@Composable
+fun ScanScreen(navController: NavController) {
+    // TODO
+}
+
+//==========================================================================================================================
+//can be placed in a file with utilities
+private fun getFileName(uri: Uri, context: Context): String {
+    var fileName = ""
+    try {
+        context.contentResolver.query(uri, null, null, null, null)?.use { cursor ->
+            val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+            if (cursor.moveToFirst() && nameIndex != -1) {
+                fileName = cursor.getString(nameIndex)
+            }
+        }
+    } catch (e: Exception) {
+        Log.e("FileUtils", "Ошибка при получении имени файла", e)
     }
+    return fileName
+}
+//==========================================================================================================================
